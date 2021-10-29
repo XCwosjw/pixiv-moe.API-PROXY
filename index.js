360581ea38d7041b5c56b86f6f8ec46d72635843
XCwosjw <3277221996@qq.com>
2021-10-29 21:44:47 +0800

    2233

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const fastify = require('fastify')
+const path = require('path')
+
+function build () {
+  const server = fastify({
+    logger: true
+})
+
+server.register(require('fastify-cors'))
+server.register(require('fastify-http-proxy'), {
+  upstream: 'https://api.pixiv.moe',
+  http2: false // optional
+})
+return server
+}
+module.exports = build
